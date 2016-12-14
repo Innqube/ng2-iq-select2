1c9a239fbed29243d976898f69701ab3beb22db7
diego <diegofsza@gmail.com>
2016-12-14 18:16:43 -0300

    Fixed export

--- component.ts
+++ component.ts
@@ -1,2 +1,4 @@
 export { IqSelect2Module } from './src/app/iq-select2.module';
 export { IqSelect2Component } from './src/app/iq-select2/iq-select2.component';
+export { IqSelect2Item } from './src/app/iq-select2/iq-select2-item';
+export { IqSelect2ResultsComponent } from './src/app/iq-select2-results/iq-select2-results.component';
