89d6b50eb2c04510d5e53b10ac0e1dfde9e68d78
diegofsza <diegofsza@gmail.com>
2017-01-13 13:36:17 -0300

    Changed BrowserModule by CommonModule (see https://angular.io/docs/ts/latest/cookbook/ngmodule-faq.html). Should resolve ISSUE #7.

--- src/app/iq-select2.module.ts
+++ src/app/iq-select2.module.ts
@@ -1,28 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
-
-import { IqSelect2Component } from './iq-select2/iq-select2.component';
-import { IqSelect2ResultsComponent } from './iq-select2-results/iq-select2-results.component';
+import {IqSelect2Component} from './iq-select2/iq-select2.component';
+import {IqSelect2ResultsComponent} from './iq-select2-results/iq-select2-results.component';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
-  declarations: [
-    IqSelect2Component,
-    IqSelect2ResultsComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    ReactiveFormsModule
-  ],
-  exports: [
-    IqSelect2Component,
-    IqSelect2ResultsComponent
-  ]
+    declarations: [
+        IqSelect2Component,
+        IqSelect2ResultsComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        HttpModule,
+        ReactiveFormsModule
+    ],
+    exports: [
+        IqSelect2Component,
+        IqSelect2ResultsComponent
+    ]
 })
-export class IqSelect2Module { }
+export class IqSelect2Module {
+}
