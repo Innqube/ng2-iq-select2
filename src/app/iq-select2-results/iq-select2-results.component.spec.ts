ebe2e4243d404a67197331ba0a062b4f9e955d6d
diegofsza <diegofsza@gmail.com>
2017-01-21 14:15:43 -0300

    Added tests

--- src/app/iq-select2-results/iq-select2-results.component.spec.ts
+++ src/app/iq-select2-results/iq-select2-results.component.spec.ts
@@ -1,29 +1,69 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
-import { IqSelect2ResultsComponent } from './iq-select2-results.component';
-import { IqSelect2Item } from '../iq-select2/iq-select2-item';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {IqSelect2ResultsComponent} from './iq-select2-results.component';
 
 describe('IqSelect2ResultsComponent', () => {
-  let component: IqSelect2ResultsComponent;
-  let fixture: ComponentFixture<IqSelect2ResultsComponent>;
+    let component: IqSelect2ResultsComponent;
+    let fixture: ComponentFixture<IqSelect2ResultsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ IqSelect2ResultsComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [IqSelect2ResultsComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(IqSelect2ResultsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(IqSelect2ResultsComponent);
+        component = fixture.componentInstance;
+        component.items = [
+            {
+                id: '1',
+                text: 'test 1'
+            },
+            {
+                id: '2',
+                text: 'test 2'
+            }];
+        component.selectedItems = [];
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should navigate to the next item', () => {
+        component.activeNext();
+        expect(component.activeIndex).toBe(1);
+    });
+
+    it('should remain at the end', () => {
+        component.activeIndex = 1;
+        component.activeNext();
+        expect(component.activeIndex).toBe(1);
+    });
+
+    it('should remain at the beginning', () => {
+        component.activePrevious();
+        expect(component.activeIndex).toBe(0);
+    });
+
+    it('should navigate to the previous item', () => {
+        component.activeIndex = 1;
+        component.activePrevious();
+        expect(component.activeIndex).toBe(0);
+    });
+
+    it('should call scrollToElement on getting to the next result', () => {
+        spyOn(component, 'scrollToElement');
+        component.activeNext();
+        expect(component.scrollToElement).toHaveBeenCalled();
+    });
+
+    it('should call scrollToElement on getting to the previous result', () => {
+        spyOn(component, 'scrollToElement');
+        component.activePrevious();
+        expect(component.scrollToElement).toHaveBeenCalled();
+    });
 });
